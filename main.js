7d1a985361ac6c65fdc0f64a71bcdd37383aae32
MeghuPatel <87625122+MeghuPatel@users.noreply.github.com>
2022-08-18 11:24:03 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+song=""
+song2=""
+leftWristX=0;
+leftWristY=0;
+rigthWristX=0;
+rigthWristY=0;
+
+function preload()
+{
+    song = loadSound("Ninjago.mp3");
+    song2 = loadSound("Harry Potter.mp3");
+}
+
+function setup()
+{
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() 
+{
+    console.log('PoseNet Is Initialize');
+}
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log('leftWristX = ' + leftWristX + "LeftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+    }
+}
